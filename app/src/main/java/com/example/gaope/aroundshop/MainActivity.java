e048809ab12f815bf66cdf12f3bf1a46eb5c46f4
gaopei <gaopeigpgp@126.com>
2018-03-12 23:00:54 +0800

    app开始的动画

--- app/src/main/java/com/example/gaope/aroundshop/MainActivity.java
+++ app/src/main/java/com/example/gaope/aroundshop/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.gaope.aroundshop;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
+
+import com.example.gaope.aroundshop.view.Main2Activity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        initImage();
+
     }
+
+    private void initImage() {
+        iv_start = (ImageView) findViewById(R.id.iv_start);
+        iv_start.setImageResource(R.drawable.enter);
+        //进行缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.4f, 1.0f, 1.4f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(4000);
+        //动画播放完成后保持形状
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                //可以在这里先进行某些操作
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        iv_start.startAnimation(scaleAnimation);
+    }
+
 }
---
+++ app/src/main/java/com/example/gaope/aroundshop/view/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.gaope.aroundshop.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.gaope.aroundshop.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
